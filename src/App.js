0474066a8b61e0b63bb1181e27644a6b19f8b13b
swapnilmanew <swapnilmanew@gmail.com>
2021-06-13 00:12:08 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Post from './components/Post';
 const App = () => {
   return (
     <div className="container">
+      <h1 className="text-center fw-bold display-4 mt-4">100 Post React Api</h1>
       <div className="row">
         <Post />
       </div>
